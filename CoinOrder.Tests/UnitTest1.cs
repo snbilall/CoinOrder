651ecd18a34de68952bdebd55404b033d60cc3d3
abdurrehimsen <abdurrehimsen@gmail.com>
2023-01-01 16:38:47 +0300

    fix

---
+++ CoinOrder.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CoinOrder.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
