034dc66b84188258e347645a75c0aaec219ca533
abdurrehimsen <abdurrehimsen@gmail.com>
2023-01-01 16:36:23 +0300

    migraton related

--- CoinOrderApi/Migrations/20230101122045_CreateTables.Designer.cs
+++ CoinOrderApi/Migrations/20230101122045_CreateTables.Designer.cs
@@ -4,6 +4,7 @@ using CoinOrderApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CoinOrderApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230101122045_CreateTables")]
+    partial class CreateTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
